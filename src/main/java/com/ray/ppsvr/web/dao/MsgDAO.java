88096eaefa27ecbf6a8b502f8d36b8256ac8e9f7
hrwhat <hrwhat214@163.com>
2014-08-06 17:33:22 +0800

    1

---
+++ src/main/java/com/ray/ppsvr/web/dao/MsgDAO.java
@@ -0,0 +1,17 @@
+package com.ray.ppsvr.web.dao;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * User: Ray
+ * Date: 2014/7/30
+ * Time: 11:06
+ */
+public interface MsgDAO {
+
+    long countMsgs();
+    List queryMsgs(Map<String, Object> params);
+
+
+}
